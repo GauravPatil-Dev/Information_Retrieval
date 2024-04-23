21708d2216a28eb583ffc4392d9021f1f978db6f
GauravPatil-Dev <39024233+GauravPatil-Dev@users.noreply.github.com>
2024-04-22 22:32:25 -0500

    Delete new directory

--- new/spiders/example.py
+++
@@ -1,9 +0,0 @@
-import scrapy
-
-class SimpleSpider(scrapy.Spider):
-    name = 'simple'
-    start_urls = ['https://www.iit.edu/']
-
-    def parse(self, response):
-        self.logger.info('Got successful response from {}'.format(response.url))
-        # Just log response, do not parse
